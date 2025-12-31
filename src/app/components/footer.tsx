ccbbbe15ca5e1aabc11e98feffaab80db8c92084
DJzmoot <NickManganielloIT@gmail.com>
2025-12-31 13:26:11 -0500

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -50,7 +50,7 @@ export function Footer() {
               <img
                 src="/stratora-logo.jpg"
                 alt="Stratora logo"
-                className="h-8 w-auto"
+                className="h-10 md:h-12 w-auto"
               />
             </div>
 
